db38901f5fc23d060209cddf6fc5aa3eff3f30cb
Nachiketa Dhal <bnachiketa26@gmail.com>
2021-09-02 20:04:41 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+import Calculator from "./components/Calculator";
+import "./_app.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <div>
+        <Calculator />
+      </div>
+    </Main>
   );
 }
+
+const Main = styled.main`
+  display: grid;
+  place-items: center;
+  min-height: 100vh;
+  div {
+    width: 30%;
+    height: 70vh;
+    background-color: #061017;
+    border-radius: 0.5em;
+  }
+`;
 
 export default App;
---
+++ src/components/Calculator.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+import "./_calculator.scss";
+
+const Calculator = () => {
+  return <Container>Calculator</Container>;
+};
+
+const Container = styled.section`
+  color: #f2f3f4;
+`;
+
+export default Calculator;
